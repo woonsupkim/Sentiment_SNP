2ffdb326380ea5d53d1cf69888bbf5f9bc2c7c3a
Woon Kim <dwkim1212@gmail.com>
2023-04-26 16:16:55 -0400

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -51,3 +51,86 @@ def count_words(string):
         if i == " ":
             count = count + 1
     return count
+
+
+
+# Fuction to collect the news doc via infinite scrolling
+from bs4 import BeautifulSoup
+import time
+from selenium import webdriver
+
+def get_page(url):
+    """Download a webpage and return a beautiful soup doc"""
+    ##### Web scrapper for infinite scrolling page #####
+    driver = webdriver.Chrome(executable_path=r"E:\Chromedriver\chromedriver_win32_chrome83\chromedriver.exe")
+    driver.get(url)
+    time.sleep(2)  # Allow 2 seconds for the web page to open
+    scroll_pause_time = 1 # You can set your own pause time. My laptop is a bit slow so I use 1 sec
+    screen_height = driver.execute_script("return window.screen.height;")   # get the screen height of the web
+    i = 1
+
+    while True:
+        # scroll one screen height each time
+        driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))  
+        i += 1
+        time.sleep(scroll_pause_time)
+        # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
+        scroll_height = driver.execute_script("return document.body.scrollHeight;")  
+        # Break the loop when the height we need to scroll to is larger than the total scroll height
+        if i == 20:
+            break 
+
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    return soup
+
+
+
+# Function to extract news tags from the doc
+
+def get_news_tags(doc):
+    """Get the list of tags containing news information"""
+    news_class = "Ov(h) Pend(44px) Pstart(25px)" ## class name of div tag 
+    news_list  = doc.find_all('div', {'class': news_class})
+    return news_list
+
+
+# Parsing news data from the news tags
+
+def parse_news(news_tag):
+    """Get the news data point and return dictionary"""
+    news_source = news_tag.find_all('span')[0].text #source
+    news_time = news_tag.find_all('span')[1].text #link
+    news_headline = news_tag.find('a').text #heading
+    news_content = news_tag.find('p').text #content
+    news_image = news_tag.findParent().find('img')['src'] #thumb image
+    return { 'source' : news_source,
+            'time' : news_time,    
+            'headline' : news_headline,
+            'content' : news_content,
+           }
+
+
+# Function to utilize all the above functions and organize the collected data into a dataframe
+import pandas as pd
+
+def scrape_yahoo_news(url, path=None):
+    """Get the yahoo finance market news and write them to CSV file """
+    if path is None:
+        path = 'stock-market-news.csv'
+        
+    #print('Requesting html page')
+    doc = get_page(url)
+
+    #print('Extracting news tags')
+    news_list = get_news_tags(doc)
+
+    #print('Parsing news tags')
+    news_data = [parse_news(news_tag) for news_tag in news_list]
+
+    #print('Save the data to a CSV')
+    news_df = pd.DataFrame(news_data)
+    #news_df.to_csv(path, index=None)
+    
+    #This return statement is optional, we are doing this just analyze the final output 
+    return news_df 
